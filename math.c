b22d7f20d384c0a7f5ea3e615d80936787ddb644
ttrubitsin <timur.trubitsin@yandex.ru>
2026-05-27 21:14:37 +0300

    variables and math in C

---
+++ math.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 7;
+	int y = 2;
+
+	printf("Сумма: %d\n", x + y);
+    	printf("Разность: %d\n", x - y);
+    	printf("Произведение: %d\n", x * y);
+    	printf("Частное: %d\n", x / y);
+    	printf("Остаток: %d\n", x % y);
+    	return (0);
+}
---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 10;
+    int b = 3;
+
+    printf("Сумма: %d\n", a + b);
+    printf("Разность: %d\n", a - b);
+    printf("Произведение: %d\n", a * b);
+    printf("Частное: %d\n", a / b);
+    printf("Остаток: %d\n", a % b);
+    return (0);
+}
