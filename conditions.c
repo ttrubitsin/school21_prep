2e413e2ee64a8b6c541a7cb727ff50f6bbaf0e3f
ttrubitsin <timur.trubitsin@yandex.ru>
2026-05-27 22:08:06 +0300

    conditions if/else

---
+++ conditions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 15;
+
+    if (age >= 18)
+    {
+        printf("Adult\n");
+    }
+    else
+    {
+        printf("Minor\n");
+    }
+    return (0);
+}
---
+++ grade.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int score = 65;
+
+    if (score >= 90)
+{
+    printf("Excellent\n");
+}
+else if (score >= 70)
+{
+    printf("Good\n");
+}
+else
+{
+    printf("Fail\n");
+}
+    return (0);
+}
